c75b56c40815930e4bc5e7e21f7aa94dccb50270
Biswajit Ghosh <bg5050525@gmail.com>
2024-01-03 19:40:16 +0530

    adding more

--- adminpanel/src/Product/Product_View.js
+++ adminpanel/src/Product/Product_View.js
@@ -1,17 +1,28 @@
-import React, { useEffect } from 'react'
-import {useSelector} from 'react-redux'
-import {useNavigate, useParams} from 'react-router-dom'
+import React, { Profiler, useEffect, useState } from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {Link, useNavigate, useParams} from 'react-router-dom'
+import {usermethod} from '../redux/userslice'
+import {selectProductmethod} from '../redux/selectProduct'
+import { PulseLoader ,BeatLoader ,ClipLoader} from 'react-spinners';
+import './Product_view.css'
 const api=process.env.REACT_APP_API
 const Product_View=()=>{
+    const dispatch=useDispatch();
     const _id=useParams()._id
     const userinfo=useSelector((state)=>state.user.user)
+    const product=useSelector((state)=>state.selectProduct.selectproduct)
+    console.log(product)
+    const [load,setload]=useState(false)
+
     const history=useNavigate()
+
     useEffect(()=>{
         loadproduct();
     },[])
 
     function loadproduct()
     {
+        setload(true)
         fetch(`${api}/product/${_id}`,{
             headers:{
                 Authorization:`Bearer ${userinfo.accessToken}`
@@ -19,11 +30,13 @@ const Product_View=()=>{
         }).then((res)=>res.json()).then((data)=>{
             if(data.statusCode==201)
             {
-                
+                dispatch(selectProductmethod.ADD_PRODUCT(data.data))
+                setload(false)
             }
             else if(data.statusCode==498)
             {
-
+                dispatch(usermethod.LOGOUT())
+                history('/')
             }
             else
             {
@@ -35,7 +48,88 @@ const Product_View=()=>{
     }
 
     return(
-        <>Product_View</>
+        <>
+        {
+            load==true?
+            <div className="Loaderitem">
+               <PulseLoader color="#16A085"  />
+            </div>
+            :product!=null &&
+            <>
+                <div className='imagedata'>
+                    <div className='imagedata1'>
+                        <Link to={`${product.newImage[0]}`} target="_blank"><img src={product.newImage[0]}  style={{width:'350px' ,height:'350px'}} alt='Error to Load'/></Link>
+                        <input type="file" placeholder='Update Image' />
+                    </div>
+                    <div className='imagedata1'>
+                        <Link to={`${product.newImage[0]}`} target="_blank"><img src={product.newImage[1]}  style={{width:'350px' ,height:'350px'}} alt='Error to Load'/></Link>
+                        <Link>Update</Link>
+                    </div>
+                    <div className='imagedata1'>
+                        <Link to={`${product.newImage[0]}`} target="_blank"><img src={product.newImage[2]}  style={{width:'350px' ,height:'350px'}} alt='Error to Load'/></Link>
+                        <Link>Update</Link>
+                    </div>
+                </div>
+                <div className='imagedata mt-5'>
+                    <div className='imagedata1'>
+                        <label>Id</label>
+                         <input value={product._id} disabled={true}/>
+                    </div>
+                    <div className='imagedata1'>
+                        <label>Product Name</label>
+                        <input value={product.product_name}/>
+                    </div>
+                    <div className='imagedata1'>
+                        <label>Product Type</label>
+                        <input value={product.product_type}/>
+                    </div>
+                    <div className='imagedata1'>
+                        <label>Rating</label>
+                        <input value={product.rating}/>
+                    </div>
+                </div>
+                <div className='imagedata mt-3'>
+                    <div className='imagedata1'>
+                        <label>Number Of Peopel Given Rating</label>
+                         <input value={product.number_of_people_give_rating}/>
+                    </div>
+                    <div className='imagedata1'>
+                        <label>Offer</label>
+                         <input value={product.offer}/>
+                    </div>
+                    <div className='imagedata1'>
+                        <label>Price</label>
+                        <input value={product.price}/>
+                    </div>
+                    <div className='imagedata1'>
+                        <label>Number of Aviliable Product</label>
+                        <input value={product.total_number_of_product}/>
+                    </div>
+                </div>
+                <div className='imagedata mt-3'>
+                   <div className='imagedata1'>
+                      <label>CreatedAt</label>
+                       <input value={product.createdAt} disabled={true} />
+                   </div>
+                   <div className='imagedata1'>
+                       <label>Last Updated At</label>
+                       <input value={product.updatedAt} disabled={true} />
+                   </div>
+                   <div className='imagedata1'>
+                       <label>Product Deleted</label>
+                       <input value={product.isdeleted}  />
+                   </div>
+                   <div className='imagedata1'>
+                      <label>Description</label>
+                      <textarea value={product.Description}/>
+                   </div>
+                </div>
+                <div className='imagedata mx-5 mt-4'>
+                    <button>Final Update</button>
+                </div>
+            </>
+        }
+        </>
     )
 }
 export default Product_View
