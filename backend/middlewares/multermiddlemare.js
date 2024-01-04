81e0783609da8bd07808ef2fac03940a04cafc33
Biswajit Ghosh <bg5050525@gmail.com>
2024-01-04 11:30:22 +0530

    adding more

--- backend/middlewares/multermiddlemare.js
+++ backend/middlewares/multermiddlemare.js
@@ -1,38 +1,16 @@
-// let multer= require("multer");
-
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, "./public/temp")
-//     },
-//     filename: function (req, file, cb) {
-//       cb(null, file.originalname)
-//     }
-// })
-  
-// const upload = multer({ 
-//     storage, 
-// })
-
-// module.exports=upload
-
 let multer= require("multer");
-let fs =require("fs");
-
-const destination = "./public/temp";
-
-// Check if the destination directory exists, create it if not
-if (!fs.existsSync(destination)) {
-  fs.mkdirSync(destination, { recursive: true });
-}
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, destination);
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+})
+  
+const upload = multer({ 
+    storage, 
+})
 
-const upload = multer({ storage });
 module.exports=upload
