17c2050a8dcc07a06841f2155164a45b6778bfa4
Biswajit Ghosh <bg5050525@gmail.com>
2024-01-09 22:54:36 +0530

    adding mre

--- frontend/src/component/Login.js
+++ frontend/src/component/Login.js
@@ -216,7 +216,7 @@ function OTPVerified()
         {
           alert(result.message)
           localStorage.setItem("user",JSON.stringify(result.data))
-          history('/')
+          history('/Product')
         }
         else{
             setotp((prevUserData) => ({
