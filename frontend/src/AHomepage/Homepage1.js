fff5ee2cb6269f6ea662250d5a350c2b1a1b2fb3
Biswajit Ghosh <biswajitghosh@Biswajits-MacBook-Air.local>
2024-05-18 22:47:41 +0530

    adding more

--- frontend/src/AHomepage/Homepage1.js
+++ frontend/src/AHomepage/Homepage1.js
@@ -1,43 +1,43 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import data from '../constant/carousel'
 import '../css/Homepage1.css'
-const Homepage1=()=>{
-    const [index,setindex]=useState(0)
+import { FcPrevious } from "react-icons/fc";
+import { FcNext } from "react-icons/fc";
+const Homepage1 = () => {
+    const [index, setindex] = useState(0)
 
-    function next()
-    {
-        setindex((index+1)%data.length)
+    function next() {
+        setindex((index + 1) % data.length)
     }
 
-    function prev()
-    {
-        setindex((index+1)%data.length)
+    function prev() {
+        setindex((index + 1) % data.length)
     }
 
-    useEffect(()=>{
-        let x=setTimeout(() => {
+    useEffect(() => {
+        let x = setTimeout(() => {
             next()
-        }, 5000);
-        return()=>{
+        }, 3000);
+        return () => {
             clearInterval(x);
         }
-    },[index])
-    return(
+    }, [index])
+    return (
         <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
             <div className="carousel-inner">
                 {
-                    data.map((item,ind)=>(
-                        ind===index?
-                        <div className="carousel-item active" key={ind}>
-                           <img className="d-block w-100" id="image" src={item} alt="slide"/>
-                           <a className="carousel-control-prev" href="#carouselExampleControls" onClick={prev} role="button" data-slide="prev">
-                                <span className="carousel-control-prev-icon" style={{backgroundColor:"black",borderRadius:"10px"}}  aria-hidden="true"></span>
-                            </a>
-                            <a className="carousel-control-next" href="#carouselExampleControls" onClick={next} role="button" data-slide="next">
-                                <span className="carousel-control-next-icon" style={{backgroundColor:"black",borderRadius:"10px"}}  aria-hidden="true"></span>
-                            </a>
-                        </div>
-                        :""
+                    data.map((item, ind) => (
+                        ind == index ?
+                            <div className="carousel-item active" key={ind}>
+                                <img className="d-block w-100" id="image" src={item} alt="" />
+                                <a className="carousel-control-prev" onClick={prev} role="button" data-slide="prev">
+                                    <FcPrevious style={{ backgroundColor: "black", borderRadius: "10px", fontSize: '20px', cursor: 'pointer' }} />
+                                </a>
+                                <a className="carousel-control-next" onClick={next} role="button" data-slide="next">
+                                    <FcNext style={{ backgroundColor: "black", borderRadius: "10px", fontSize: '20px', cursor: 'pointer' }} />
+                                </a>
+                            </div>
+                            : ""
                     ))
                 }
             </div>
